f923e3f688fa4275d19d205437170c0d9ed92249
Abhinav A <abhinavajitsaria@gmail.com>
2023-08-05 19:53:40 +0530

    create function for addition

--- Calc.java
+++ Calc.java
@@ -1,11 +1,16 @@
 public class Calc
 {
+    public static int add(int a, int b)
+    {
+        return a+b;
+    }
+
     public static void main(String args[])
     {
         int a,b;
         a=5;
         b=6;
-        int c=a+b;
+        int c = add(a,b);
         System.out.print(c);
         
     }
