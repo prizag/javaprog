571d11fe2533b0c2be0869c26367990beb9c5bfd
Priyanka A <priyanka060905@gmail.com>
2023-08-05 13:10:15 +0530

    initial commit

---
+++ Calc.java
@@ -0,0 +1,12 @@
+public class Calc
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        a=5;
+        b=6;
+        int c=a+b;
+        System.out.print(c);
+        
+    }
+}
