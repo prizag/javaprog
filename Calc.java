59fa7c57539e125779d6806a0ff13d0ea096ee6b
Priyanka A <priyanka060905@gmail.com>
2023-08-05 12:38:59 +0530

    create function for subtraction

--- Calc.java
+++ Calc.java
@@ -1,12 +1,17 @@
 public class Calc
 {
+    public static int sub(int a, int b)
+    {
+        return a-b;
+    }
     public static void main(String args[])
     {
         int a,b;
         a=5;
         b=6;
         int c=a+b;
+        int d=sub(a,b);
         System.out.print(c);
-        
+
     }
 }
